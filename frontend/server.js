bc9334efcdeea74212a54354689301c95d89173c
suhail-asarat <38782561+suhail-asarat@users.noreply.github.com>
2026-04-09 08:31:29 +0600

    replaced the deprecated url.parse() flow with the WHATWG URL API
    (node:9808) [DEP0169] DeprecationWarning: `url.parse()` behavior is not standardized and prone to errors that have security implications. Use the WHATWG URL API instead. CVEs are not issued for `url.parse()` vulnerabilities.
    (Use `node --trace-deprecation ...` to show where the warning was created)

--- frontend/server.js
+++ frontend/server.js
@@ -1,5 +1,4 @@
 const { createServer } = require("http");
-const { parse } = require("url");
 const next = require("next");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
@@ -9,7 +8,13 @@ const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
+    const baseUrl = `http://${req.headers.host || "localhost"}`;
+    const requestUrl = new URL(req.url, baseUrl);
+    const parsedUrl = {
+      pathname: requestUrl.pathname,
+      query: Object.fromEntries(requestUrl.searchParams),
+    };
+
     handle(req, res, parsedUrl);
   }).listen(port, (err) => {
     if (err) {
